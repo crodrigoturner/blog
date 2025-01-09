8558a95e076c704c26f02d0e98f75ea7f677b2af
Carlos Rodrigo Turner <57825319+crodrigoturner@users.noreply.github.com>
2025-01-09 12:43:30 +0100

    d

--- eleventy.config.js
+++ eleventy.config.js
@@ -103,6 +103,27 @@ eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
 		}
 	});
 
+
+	eleventyConfig.addLiquidFilter("getYearlyPostCount", (posts, startYear = 2007) => {
+		let years = [];
+		for(let j = startYear; j <= (new Date()).getFullYear(); j++) {
+			let year = j;
+			let count = posts.filter(function(post) {
+				if(!post.data.tags) {
+					return true;
+				}
+				if(post.data.deprecated ||post.data.tags.includes("draft")) {
+					return false;
+				}
+				return true;
+			}).filter(function(post) {
+				return post.data.page.date.getFullYear() === parseInt(year, 10);
+			}).length;
+			years.push(count);
+		}
+		return years.join(",");
+	});
+
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
 
@@ -125,25 +146,7 @@ eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
 	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 };
 
-eleventyConfig.addLiquidFilter("getYearlyPostCount", (posts, startYear = 2007) => {
-	let years = [];
-	for(let j = startYear; j <= (new Date()).getFullYear(); j++) {
-		let year = j;
-		let count = posts.filter(function(post) {
-			if(!post.data.tags) {
-				return true;
-			}
-			if(post.data.deprecated ||post.data.tags.includes("draft")) {
-				return false;
-			}
-			return true;
-		}).filter(function(post) {
-			return post.data.page.date.getFullYear() === parseInt(year, 10);
-		}).length;
-		years.push(count);
-	}
-	return years.join(",");
-});
+
 
 
 export const config = {
