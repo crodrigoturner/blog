4e8f9eb181bbe91b8b7939ebd67fa584c8b567df
Carlos Rodrigo Turner <57825319+crodrigoturner@users.noreply.github.com>
2025-01-15 12:54:03 +0100

    d

--- eleventy.config.js
+++ eleventy.config.js
@@ -103,25 +103,7 @@ eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
 		}
 	});
 
-	eleventyConfig.addFilter("getYearlyPostCount", (posts, startYear = 1985) => {
-		let years = [];
-		for(let j = startYear; j <= (new Date()).getFullYear(); j++) {
-			let year = j;
-			let count = posts.filter(function(post) {
-				if(!post.data.tags) {
-					return true;
-				}
-				if(post.data.deprecated ||post.data.tags.includes("draft")) {
-					return false;
-				}
-				return true;
-			}).filter(function(post) {
-				return post.data.page.date.getFullYear() === parseInt(year, 10);
-			}).length;
-			years.push(count);
-		}
-		return years.join(",");
-	});
+
 
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
